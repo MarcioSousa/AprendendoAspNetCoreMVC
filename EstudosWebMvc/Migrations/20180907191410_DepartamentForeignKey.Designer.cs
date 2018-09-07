d04e8d908b41ae70c365ff03a46c542ac8ff6948
Marcio Sousa <marciofrapeso@gmail.com>
2018-09-07 16:20:18 -0300

    Foreign key not null (referential integrity)

--- EstudosWebMvc/Migrations/20180907191410_DepartamentForeignKey.Designer.cs
+++ EstudosWebMvc/Migrations/20180907191410_DepartamentForeignKey.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EstudosWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EstudosWebMvc.Migrations
 {
     [DbContext(typeof(EstudosWebMvcContext))]
-    partial class EstudosWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20180907191410_DepartamentForeignKey")]
+    partial class DepartamentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EstudosWebMvc/Services/SellerService.cs
+++ EstudosWebMvc/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace EstudosWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Departament = _context.Departament.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
