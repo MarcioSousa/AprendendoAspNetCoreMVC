b4d14a4752cd8b0d45a0ce82edc6755758b8dc8a
Marcio Sousa <marciofrapeso@gmail.com>
2018-09-07 10:49:45 -0300

    Mysql Adaptação e primeira migracao

--- EstudosWebMvc/Startup.cs
+++ EstudosWebMvc/Startup.cs
@@ -36,7 +36,8 @@ namespace EstudosWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<EstudosWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("EstudosWebMvcContext")));
+                options.UseMySql(Configuration.GetConnectionString("EstudosWebMvcContext"), builder =>
+                builder.MigrationsAssembly("EstudosWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
