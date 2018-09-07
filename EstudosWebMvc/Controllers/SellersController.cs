c50993dde7bcf791672d2db31804a398fcc8f15d
Marcio Sousa <marciofrapeso@gmail.com>
2018-09-07 19:57:42 -0300

    Update seller and custon service exception

--- EstudosWebMvc/Controllers/SellersController.cs
+++ EstudosWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using EstudosWebMvc.Services;
 using EstudosWebMvc.Models;
 using EstudosWebMvc.Models.ViewModels;
+using EstudosWebMvc.Services.Exceptions;
 
 namespace EstudosWebMvc.Controllers
 {
@@ -83,5 +84,50 @@ namespace EstudosWebMvc.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Departament> departaments = _departamentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departaments = departaments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+ _sellerService.Update(seller);
+            return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+           
+
+        }
+
     }
 }
