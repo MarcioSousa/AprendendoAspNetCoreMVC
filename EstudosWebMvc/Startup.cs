539152cce25564807a6fa0eca5e7cc89cebff131
Marcio Sousa <marciofrapeso@gmail.com>
2018-09-07 08:53:13 -0300

    CRUD Scaffolding

--- EstudosWebMvc/Startup.cs
+++ EstudosWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EstudosWebMvc.Models;
 
 namespace EstudosWebMvc
 {
@@ -32,6 +34,9 @@ namespace EstudosWebMvc
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<EstudosWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EstudosWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
